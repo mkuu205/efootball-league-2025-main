b7361461a1128b90b8d5a6bd9f60a1952f136d24
Mkuu205 <Ndegwabrian022@gmail.com>
2025-10-18 13:59:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 const PORT = process.env.PORT || 10000;
 // Middleware
 app.use(cors());
