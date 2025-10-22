e2f537a09bf61c038d3f6608d5809bc61a7ad34f
Mkuu205 <Ndegwabrian022@gmail.com>
2025-10-22 20:33:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { MongoClient } = require('mongodb');
-const sendResetEmail = require('./js/send-reset-email');
+const { Resend } = require("resend");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
