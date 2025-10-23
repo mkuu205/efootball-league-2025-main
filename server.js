61a7e9f043c64fc06ab88c45a3251b2e6de18183
Mkuu205 <Ndegwabrian022@gmail.com>
2025-10-23 05:48:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // --------------------
 // MongoDB Connection
