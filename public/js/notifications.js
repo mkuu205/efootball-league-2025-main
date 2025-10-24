cd53cc51db4fc26cf7b48c1e6d219d77042f2579
Mkuu205 <Ndegwabrian022@gmail.com>
2025-10-24 05:00:06 +0300

    Update notifications.js

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -12,7 +12,7 @@ async function registerPush() {
 
   try {
     console.log("Registering service worker...");
-    const register = await navigator.serviceWorker.register("/service-worker.js");
+    const register = await navigator.serviceWorker.register("/public/service-worker.js");
     console.log("Service Worker registered ✅");
 
     console.log("Requesting notification permission...");
