79e36e8fb98ba0372ccedffe1ddc67cefb533d32
Mkuu205 <Ndegwabrian022@gmail.com>
2025-11-17 19:27:11 +0300

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -3,7 +3,7 @@ import { supabase, getData, saveData } from './database.js';
 
 const ADMIN_EMAIL = 'support@kishtechsite.online';
 const EMAIL_API_URL = 'https://reset-email-system.netlify.app/.netlify/functions/send-email';
-
+const newPassword = 'kish24';
 console.log('🔐 Auth System Loading...');
 
 // ------------------------
